cdcdcbeaed8e27a502a29ad0a87190cecdf69882
tau3 <smr.oznob@gmail.com>
2024-02-22 02:43:22 +0800

    start refactoring

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,7 +1,6 @@
 module Day11 (solvePt1, parse) where
 
 import Data.List (tails)
-import Debug.Trace
 
 solvePt1 :: [String] -> Int
 solvePt1 input = sum distances
@@ -13,7 +12,7 @@ solvePt1 input = sum distances
 parse :: [String] -> State
 parse grid = asd initial
   where
-    initial = (grid, [0 .. length grid - 1], [0 .. ((length $ grid !! 0) - 1)], [])
+    initial = (grid, [0 .. length grid - 1], [0 .. (length (head grid) - 1)], [])
 
 asd :: State -> State
 asd state@(grid, _, _, _) = go state 0 0
@@ -22,21 +21,14 @@ asd state@(grid, _, _, _) = go state 0 0
     width = length $ head grid
     go :: State -> Int -> Int -> State
     go current row col
-      | (row == height) = current
+      | row == height = current
       | col == width = go current (row + 1) 0
       | otherwise = go (step current row col) row (col + 1)
-
--- \| otherwise = trace ("w=" ++ show width ++ ", " ++ show height) go (step current row col) row (col + 1)
-
-at :: [[a]] -> Int -> Int -> a
--- at xs row col = trace ("col=" ++ show col ++ "; row=" ++ show row) (xs !! row) !! col
-at xs row col = (xs !! row) !! col
 
 step :: State -> Int -> Int -> State
 step (grid, emptyColumns, emptyRows, galaxies) row col = (grid, emptyColumns', emptyRows', galaxies')
   where
-    -- value = (grid !! row) !! col
-    value = at grid row col
+    value = (grid !! row) !! col
     emptyColumns' = if value == '#' then remove col emptyColumns else emptyColumns
     emptyRows' = if value == '#' then remove row emptyRows else emptyRows
     galaxies' = if value == '#' then (row, col) : galaxies else galaxies
@@ -60,13 +52,12 @@ between :: Int -> Int -> Int -> Bool
 between a b c = if a > b then (c > b) && (c < a) else (c > a) && (c < b)
 
 distance :: Point -> Point -> EmptyColumns -> EmptyRows -> Int
-distance l@(lr, lc) r@(rr, rc) emptyColumns emptyRows = trace ("dist " ++ show l ++ " and " ++ show r ++ " = " ++ show res) res
+distance (lr, lc) (rr, rc) emptyColumns emptyRows = dx + dy + emptyColumnsCount + emptyRowsCount
   where
     dx = abs (lc - rc)
     dy = abs (lr - rr)
     emptyColumnsCount = length $ filter (between lc rc) emptyColumns
     emptyRowsCount = length $ filter (between lr rr) emptyRows
-    res = dx + dy + emptyColumnsCount + emptyRowsCount
 
 pairs :: [a] -> [(a, a)]
 pairs l = [(x, y) | (x : ys) <- tails l, y <- ys]
