5f6634b698c7baf4da77d8f6bfa1b161246e6f6e
tau3 <smr.oznob@gmail.com>
2024-02-22 03:16:28 +0800

    solve pt1

--- app/Main.hs
+++ app/Main.hs
@@ -7,9 +7,9 @@ import System.IO (IOMode (ReadMode), hClose, hGetContents, openFile)
 main = do
   pwd <- getCurrentDirectory
   print pwd
-  handle <- openFile "../resources/day8_input" ReadMode
+  handle <- openFile "resources/day11_input" ReadMode
   contents <- hGetContents handle
-  hClose handle
   let input = lines contents
   print $ solvePt1 input
+  hClose handle
   return ()
