279368bcf7a9da52c17c6183d63d78a891ba933f
tau3 <smr.oznob@gmail.com>
2025-12-19 09:02:38 +0700

    solve day3

--- src/Day3.hs
+++ src/Day3.hs
@@ -3,7 +3,7 @@ module Day3 (solve) where
 import qualified Data.Set as Set
 
 solve :: [String] -> Int
-solve = solvePt1 . head
+solve = solvePt2 . head
 
 nextPos :: (Int, Int) -> Char -> (Int, Int)
 nextPos (x, y) '^' = (x, y - 1)
@@ -21,3 +21,17 @@ solvePt1 line = go (0, 0) line (Set.singleton (0, 0))
       let pos' = nextPos pos direction
           visited' = Set.insert pos' visited
        in go pos' rest visited'
+
+solvePt2 :: String -> Int
+solvePt2 line = go (0, 0) (0, 0) True line (Set.singleton (0, 0))
+  where
+    go :: (Int, Int) -> (Int, Int) -> Bool -> String -> Set.Set (Int, Int) -> Int
+    go _ _ _ [] visited = Set.size visited
+    go santa robo True (direction : rest) visited =
+      let santa' = nextPos santa direction
+          visited' = Set.insert santa' visited
+       in go santa' robo False rest visited'
+    go santa robo False (direction : rest) visited =
+      let robo' = nextPos robo direction
+          visited' = Set.insert robo' visited
+       in go santa robo' True rest visited'
